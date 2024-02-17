6ba103dc2eebda64575d91b28fcdce373febb83e
walid <walidashraf050@gmail.com>
2024-02-18 00:37:56 +0200

    API Tutorials (Currency Converter)

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+// Scroll Tutorials
 let el = document.querySelector(".scroller");
 let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
 window.addEventListener("scroll", () => {
     let scrollTop = document.documentElement.scrollTop;
     el.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+// API Tutorials (Currency Converter)
+fetch("https://api.currencyfreaks.com/v2.0/rates/latest?apikey=0624c6c204234b1985582361355a21af").then((result) => {
+    let myData = result.json();
+    return myData;
+}).then((currency) => {
+    let amount = document.querySelector(".amount");
+    let egpPrice = document.querySelector(".egp span");
+    let sarPrice = document.querySelector(".sar span");
+    egpPrice.innerHTML = Math.round(amount.innerHTML * currency.rates["EGP"]);
+    sarPrice.innerHTML = Math.round(amount.innerHTML * currency.rates["SAR"]);
 })
